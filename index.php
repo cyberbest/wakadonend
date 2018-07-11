b29787a6231732e5d55422429c4f310d3cf1d1e7
cyberbest <cyberbesttechnologies@gmail.com>
2018-07-11 11:31:26 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,425 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Wakadonend - Home</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="vendor/bootstrap/css/styles.css" rel="stylesheet">
+   <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
+
+    <!-- Font-awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+    <!-- Custom styles for this template -->
+    <style>
+      body {
+        padding-top: 54px;
+      }
+      @media (min-width: 992px) {
+        body {
+          padding-top: 56px;
+        }
+      }
+
+
+    </style>
+
+  </head>
+
+  <body>
+
+
+<!-- header -->
+<header class="bg-dark">
+<div class="banner fill position-relative" style="background-image: url('bg1.jpg');">
+
+  <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg fixed-top bg-light">
+      <a class="navbar-brand" href="#"><img src="vendor/image/logo-waka.png" class=""></a>
+      <div>
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item"><h6><a class="nav-link" href="#">Market</a></h6></li>
+            <li class="nav-item"><h6><a class="nav-link" href="#">Property</a></h6></li>
+            <li class="nav-item"><h6><a class="nav-link" href="#">Advertisment</a></h6></li>
+          </ul>
+        </div>
+      <div class="container">
+        
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link " href="#"><h6>Become a Seller</h6>
+              </a>
+            </li>
+            <li class="nav-item">|</li>
+            <li class="nav-item">
+              <a class="nav-link" href="#"><h6>Become an Agent/Advertiser</h6></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active bg-danger text-white" href="#">Login <span class="sr-only">(current)</span></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+
+
+
+  <div class="col-lg-12 position-absolute fixed-bottom">
+    <div class="col-lg-7">
+      <h1>Find anything! Sale anything! any time!</h1>
+    </div>
+    <div class="search">
+      <form>
+        <div class="form-group">
+        <input type="text" name="search_waka" class="col-lg-11 form-control float-left">
+        <button class="btn btn-primary">Waka</button>
+      </div>
+      </form>
+    </div>
+    
+  </div>
+</div> 
+  
+</header>
+
+<!-- main content section -->
+<div class="main">
+  <div class="col-lg-12 bg-danger">
+    <div class="col-lg-4 text-center float-left" ><i class="fa fa-shopping-cart fa-4x" style="color: #900C3F"></i>
+<br>Market Place</div>
+    <div class="col-lg-4 text-center float-left"><i class="fa fa-home fa-4x" style="color: #900C3F"></i><br>Property</div>
+    <div class="col-lg-4 text-center float-left"><i class="fa fa-bullhorn fa-4x" style="color: #900C3F"></i><br>Advertisment</div>
+  </div>
+
+  <div class="col-lg-12 clearfix">
+    <div class="col-lg-9 float-left">
+      <h4> Trending </h4>
+      <div class="row">
+        
+        <div class="col-lg-4 col-md-6 mb-4">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="vendor/image/olives-prdt.jpg" alt=""></a>
+            <h5 class="card-title">
+              <a href="#">Item name</a>
+            </h5>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-4">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="vendor/image/olives-prdt.jpg" alt=""></a>
+            <h5 class="card-title">
+              <a href="#">Item name</a>
+            </h5>
+          </div>
+        </div>        
+        <div class="col-lg-4 col-md-6 mb-4">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="vendor/image/olives-prdt.jpg" alt=""></a>
+            <h5 class="card-title">
+              <a href="#">Item name</a>
+            </h5>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-4">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="vendor/image/olives-prdt.jpg" alt=""></a>
+            <h5 class="card-title">
+              <a href="#">Item name</a>
+            </h5>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-4">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="vendor/image/olives-prdt.jpg" alt=""></a>
+            <h5 class="card-title">
+              <a href="#">Item name</a>
+            </h5>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-4">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="vendor/image/olives-prdt.jpg" alt=""></a>
+            <h5 class="card-title">
+              <a href="#">Item name</a>
+            </h5>
+          </div>
+        </div>
+                                
+      </div>
+      <div class="col-lg-12" style="height: 100px"><img src="vendor/image/ads1.jpg"></div>
+      <h4> Market place </h4>
+      <div class="row">
+        
+        <div class="col-lg-4 col-md-6 mb-4">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="vendor/image/olives-prdt.jpg" alt=""></a>
+            <h5 class="card-title">
+              <a href="#">Item name</a>
+            </h5>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-4">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="vendor/image/olives-prdt.jpg" alt=""></a>
+            <h5 class="card-title">
+              <a href="#">Item name</a>
+            </h5>
+          </div>
+        </div>        
+        <div class="col-lg-4 col-md-6 mb-4">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="vendor/image/olives-prdt.jpg" alt=""></a>
+            <h5 class="card-title">
+              <a href="#">Item name</a>
+            </h5>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-4">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="vendor/image/olives-prdt.jpg" alt=""></a>
+            <h5 class="card-title">
+              <a href="#">Item name</a>
+            </h5>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-4">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="vendor/image/olives-prdt.jpg" alt=""></a>
+            <h5 class="card-title">
+              <a href="#">Item name</a>
+            </h5>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-4">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="vendor/image/olives-prdt.jpg" alt=""></a>
+            <h5 class="card-title">
+              <a href="#">Item name</a>
+            </h5>
+          </div>
+        </div>
+
+      </div>
+      <div class="col-lg-12" style="height: 100px;"><img src="vendor/image/ads1.jpg"></div>
+      <h4> Property </h4>
+      <div class="row">
+        
+        <div class="col-lg-4 col-md-6 mb-4">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="vendor/image/olives-prdt.jpg" alt=""></a>
+            <h5 class="card-title">
+              <a href="#">Item name</a>
+            </h5>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-4">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="vendor/image/olives-prdt.jpg" alt=""></a>
+            <h5 class="card-title">
+              <a href="#">Item name</a>
+            </h5>
+          </div>
+        </div>        
+        <div class="col-lg-4 col-md-6 mb-4">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="vendor/image/olives-prdt.jpg" alt=""></a>
+            <h5 class="card-title">
+              <a href="#">Item name</a>
+            </h5>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-4">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="vendor/image/olives-prdt.jpg" alt=""></a>
+            <h5 class="card-title">
+              <a href="#">Item name</a>
+            </h5>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-4">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="vendor/image/olives-prdt.jpg" alt=""></a>
+            <h5 class="card-title">
+              <a href="#">Item name</a>
+            </h5>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-4">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="vendor/image/olives-prdt.jpg" alt=""></a>
+            <h5 class="card-title">
+              <a href="#">Item name</a>
+            </h5>
+          </div>
+        </div>
+                                
+      </div>    
+
+    </div>
+
+    <div class="col-lg-3 float-right" style="padding-bottom: 10px">
+      <div class="col-lg-12 clearfix" style="margin-top:10px">
+        <div class="col-lg-4 bg-danger float-left">
+          <div class="row">
+            <img src="vendor/image/thums.jpg">
+          </div>
+        </div>
+          <div class="col-lg-8 bg-danger float-right" style="height: 66px;">This is a text paragraph for the last post item</div>
+      </div>
+
+      <div class="col-lg-12 clearfix" style="margin-top:10px">
+        <div class="col-lg-4 bg-danger float-left">
+          <div class="row">
+            <img src="vendor/image/thums.jpg">
+          </div>
+        </div>
+          <div class="col-lg-8 bg-danger float-right" style="height: 66px;">This is a text paragraph for the last post item</div>
+      </div>
+      
+      <div class="col-lg-12 clearfix" style="margin-top:10px">
+        <div class="col-lg-4 bg-danger float-left">
+          <div class="row">
+            <img src="vendor/image/thums.jpg">
+          </div>
+        </div>
+          <div class="col-lg-8 bg-danger float-right" style="height: 66px;">This is a text paragraph for the last post item</div>
+      </div>
+
+      <div class="col-lg-12 bg-primary text-white" style="margin-top: 10px; margin-bottom: 10px">
+        <h5>Search by Location</h5>
+        <form class="form-control">
+          <div class="form-group">
+            <input type="text" name="search" class="form-control" placeholder="chose category...">
+          </div>
+          <div class="form-group">
+            <input type="text" name="search" class="form-control" placeholder="chose state...">
+          </div>
+          <div class="form-group">
+            <input type="text" name="search" class="form-control" placeholder="chose city...">
+          </div>
+          <div class="form-group">
+            <input type="text" name="search" class="form-control" placeholder="chose area...">
+          </div>
+          <button class="btn btn-success form-control">WAKA</button>
+        </form>        <p>&nbsp;</p>
+
+      </div>
+      <div class="col-lg-12 bg-danger text-white">
+        <h5> Adds space</h5>
+        <div class="row">
+          <h1>opp</h1>
+          <img src="">
+        </div>
+      </div>
+    
+      
+
+
+      
+    Working
+    </div>
+    </div>
+
+  </div>
+
+  <div class="col-12-lg-12">
+    <h4 class="card-title">How it works</h4>
+    <p class="description">
+      Learn how to build and grow your business with Wakadonend
+    </p>
+  </div>
+
+
+
+
+  <div class="col-lg-12 ">
+    <h1>contents here</h1>
+  </div>
+</div>
+
+<!-- footer section -->
+<footer class="bg-primary">
+
+  <div class="col-lg-12">
+    <p>&nbsp;</p>
+    <div class="col-lg-3 float-left" style="height: 220px">
+      <h6 class="text-white">Categories</h6>
+        <ul>
+          <li>work</li>
+          <li>work</li>
+          <li>work</li>
+          <li>work</li>
+          <li>work</li>
+          <li>work</li>
+          <li>work</li>
+        </ul>
+    </div>
+    <div class="col-lg-3 float-left" style="height: 220px">
+      <h6 class="text-white">Quick links</h6>
+        <ul>
+          <li>work</li>
+           <li>work</li>
+           <li>work</li>
+           <li>work</li>
+           <li>work</li>
+           <li>work</li>
+           <li>work</li>
+          </ul>
+    </div>
+    <div class="col-lg-3 float-left" style="height: 220px">
+      <h6 class="text-white">Need help</h6>
+        <ul>
+          <li>work</li>
+           <li>work</li>
+           <li>work</li>
+           <li>work</li>
+        </ul>
+      <h6 class="text-white">Earn cash with us</h6>
+        <ul>
+          <li>work</li>
+          <li>work</li>
+        </ul>
+    </div>
+    <div class="col-lg-3 float-left" style="height: 220px">
+      <h6 class="text-white">Reach us</h6> 
+        <ul>
+          <li>work</li>
+          <li>work</li>
+          <li>work</li>
+          <li>work</li> 
+        </ul>
+      <h6 class="text-white">Social medie</h6>
+        <a href="#" class="text-dark"><i class="fa fa-facebook fa-2x"></i></a>
+        <a href="#" class="text-dark"><i class="fa fa-twitter fa-2x"></i></a>
+        <a href="#" class="text-dark"><i class="fa fa-google-plus fa-2x"></i></a>
+    </div>
+    <p>&nbsp;</p>  
+  </div>
+
+ <div class="col-lg-12 bg-dark text-center">
+  <span class="text-white">Wakadonend.com &copy; 2018 Copy Right. All Right Reserved</span>
+</div>
+
+</footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
